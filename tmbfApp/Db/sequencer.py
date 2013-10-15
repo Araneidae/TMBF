8037b71f300897c2ae4f9ad55cf592bf6ab8e88f
mga83 <mga83@e099a375-04f9-0310-9d5f-a741eaff62e1>
2013-10-15 09:12:49 +0000

    TMBF: Update sequencer on every field change
    Although this does generate a *lot* of writes, this doesn't matter,
    and it turns out to be an enormous pain having to separately update
    fields and then write the sequencer.  This feature is now removed.
    
    git-svn-id: svn+ssh://serv2/home/subversion/repos/controls/diamond/trunk/ioc/TMBF@100397 e099a375-04f9-0310-9d5f-a741eaff62e1

--- tmbfApp/Db/sequencer.py
+++ tmbfApp/Db/sequencer.py
@@ -6,36 +6,26 @@ from common import *
 # The sequencer has eight possible states, however state 0 has somewhat
 # different behaviour from the remaining 7.
 
-
-validity = records.bi('SEQ:VALID',
-    ZNAM = 'Needs Write',   ZSV  = 'MINOR',
-    ONAM = 'Written',       OSV  = 'NO_ALARM')
-invalidate = records.bo('SEQ:INVALIDATE', OUT = PP(validity), VAL = 0)
-make_valid = records.bo('SEQ:MAKE_VALID', OUT = PP(validity), VAL = 1)
+update = boolOut('SEQ:WRITE', DESC = 'Write sequencer settings')
 for state in range(1, 8):
     aOut('SEQ:%d:START_FREQ' % state, -936, 936, 'tune', 5,
-        FLNK = invalidate, DESC = 'Sweep NCO start frequency')
+        FLNK = update, DESC = 'Sweep NCO start frequency')
     aOut('SEQ:%d:STEP_FREQ' % state, -936, 936, 'tune', 7,
-        FLNK = invalidate, DESC = 'Sweep NCO step frequency')
+        FLNK = update, DESC = 'Sweep NCO step frequency')
     longOut('SEQ:%d:DWELL' % state, 1, (1<<31) - 1,
-        FLNK = invalidate, EGU = 'turns', DESC = 'Sweep dwell time')
+        FLNK = update, EGU = 'turns', DESC = 'Sweep dwell time')
     longOut('SEQ:%d:COUNT' % state, 1, 1<<12,
-        FLNK = invalidate, DESC = 'Sweep count')
+        FLNK = update, DESC = 'Sweep count')
     mbbOut('SEQ:%d:BANK' % state, 'Bank 0', 'Bank 1', 'Bank 2', 'Bank 3',
-        FLNK = invalidate, DESC = 'Bunch bank selection')
-    mbbOut('SEQ:%d:GAIN' % state, DESC = 'Sweep NCO gain', FLNK = invalidate,
+        FLNK = update, DESC = 'Bunch bank selection')
+    mbbOut('SEQ:%d:GAIN' % state, DESC = 'Sweep NCO gain', FLNK = update,
         *dBrange(8, -6))
     boolOut('SEQ:%d:ENWIN' % state, 'Disabled', 'Enabled',
-        FLNK = invalidate, DESC = 'Enable detector window')
+        FLNK = update, DESC = 'Enable detector window')
 
 # This is the only valid control in state 0.
 mbbOut('SEQ:0:BANK', 'Bank 0', 'Bank 1', 'Bank 2', 'Bank 3',
     DESC = 'Bunch bank selection')
-
-# Note the use of PINI='RUN' -- this triggers PINI processing later than all
-# the saved states above, ensuring that we write the correct saved state.
-boolOut('SEQ:WRITE',
-    PINI = 'RUN', FLNK = make_valid, DESC = 'Write sequencer settings')
 
 longOut('SEQ:PC', 0, 7, DESC = 'Sequencer PC')
 longIn('SEQ:PC', DESC = 'Current sequencer state', SCAN = '.1 second')
