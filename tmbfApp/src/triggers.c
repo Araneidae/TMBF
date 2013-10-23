7de6afcce2258770f9b6841df2d77eaac2497041
mga83 <mga83@e099a375-04f9-0310-9d5f-a741eaff62e1>
2013-10-23 13:25:50 +0000

    TMBF: Rewrite of triggers.c to avoid trigger lockup
    The separation between trigger source and targets is confusing and
    slightly confused.  This rewrite is an attempt to avoid a rare bug
    where a trigger source becomes stuck in the Busy state.
    
    git-svn-id: svn+ssh://serv2/home/subversion/repos/controls/diamond/trunk/ioc/TMBF@100741 e099a375-04f9-0310-9d5f-a741eaff62e1

--- tmbfApp/src/triggers.c
+++ tmbfApp/src/triggers.c
@@ -38,70 +38,158 @@ enum {
 };
 
 
-
-/* Records state of a virtual trigger source (soft or external). */
-struct trigger_source {
-    unsigned int source;        // Source identification
-    bool retrigger;             // If auto-retrigger enabled
-
-    /* The following three flags record which events we're waiting for.  Once
-     * they all fire we can retrigger. */
-    bool ddr_armed;
-    bool buf_armed;
-    bool seq_armed;
-
-    /* We report as armed until all of the events above have reported firing. */
+/* Used for reporting armed status. */
+struct armed_status {
     bool armed;
     struct epics_record *status_pv;
 };
 
 
-/* Records status of a trigger target. */
-struct target_status {
-    bool armed;             // If armed and waiting for completion
-    struct epics_record *status_pv;
-    struct trigger_source *current_source;
+/* Records state of a virtual trigger source (soft or external). */
+struct trigger_source {
+    unsigned int source_id;     // Source identification
+    bool retrigger;             // If auto-retrigger enabled, written from PV
+    struct armed_status armed;  // Aggregate status of all our targets
+
+    /* We keep track of which targets this source triggered last time it fired
+     * so that we can refresh our status as appropriate. */
+    bool ddr_armed;
+    bool buf_armed;
+    bool seq_armed;
 };
 
 
 /* Records state of a trigger target (DDR or fast buffer). */
-struct trigger_target {
-    struct target_status status;
-    /* Identifies physical target. */
-    unsigned int source;    // Configured source (invalid for sequencer)
-    void (*notify)(void);    // Notifies arming event
+struct ddr_target {
+    struct armed_status armed;
+    unsigned int source_select; // Configured source, written from PV
+    bool one_shot;
+};
+
+struct buf_target {
+    struct armed_status armed;
+    unsigned int source_select; // Configured source, written from PV
+};
+
+/* The sequencer is a little different as it shares triggering with the
+ * buffer, but has its own enable state. */
+struct seq_target {
+    struct armed_status armed;
+    bool enabled;               // Updated from PV
 };
 
 
 
-static struct trigger_source s1_source  = { .source = TRIG_SOURCE_SOFT1 };
-static struct trigger_source s2_source  = { .source = TRIG_SOURCE_SOFT2 };
-static struct trigger_source ext_source = { .source = TRIG_SOURCE_EXTERNAL };
+static struct trigger_source s1_source  = { .source_id = TRIG_SOURCE_SOFT1 };
+static struct trigger_source s2_source  = { .source_id = TRIG_SOURCE_SOFT2 };
+static struct trigger_source ext_source = { .source_id = TRIG_SOURCE_EXTERNAL };
 
-static struct trigger_target ddr_target = { .notify = arming_ddr_buffer };
-static struct trigger_target buf_target;
-
-static struct target_status seq_target;
-static bool seq_enable;
+static struct ddr_target ddr_target;
+static struct buf_target buf_target;
+static struct seq_target seq_target;
 
 
-/* Some forward declarations as source and target form a dependency loop. */
-static bool maybe_arm_target(
-    struct trigger_target *target, struct trigger_source *source);
 
-static bool maybe_arm_sequencer(struct trigger_source *source);
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/* Armed status reporting. */
+
+/* Initialises armed_status structure including publishing of associated PV. */
+static void publish_status(const char *name, struct armed_status *status)
+{
+    status->armed = false;
+    status->status_pv = PUBLISH_READ_VAR_I(bi, name, status->armed);
+}
+
+
+/* Updates armed status and generates PV update if necessary. */
+static void set_armed_status(struct armed_status *status, bool armed)
+{
+    if (armed != status->armed)
+    {
+        status->armed = armed;
+        trigger_record(status->status_pv, 0, NULL);
+    }
+}
+
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/* Trigger targets (DDR/BUF/SEQ). */
+
+
+static void do_arm_ddr(bool external, bool one_shot)
+{
+    set_armed_status(&ddr_target.armed, true);
+    ddr_target.one_shot = one_shot;
+
+    hw_write_ddr_trigger_select(external);
+    arming_ddr_buffer();
+}
+
+static void disarm_ddr(void)
+{
+    set_armed_status(&ddr_target.armed, false);
+}
+
+
+static void do_arm_buf(bool external)
+{
+    set_armed_status(&buf_target.armed, true);
+    hw_write_buf_trigger_select(external);
+}
+
+static void disarm_buf(void)
+{
+    set_armed_status(&buf_target.armed, false);
+}
+
+
+static void maybe_arm_seq(void)
+{
+    set_armed_status(&seq_target.armed, seq_target.enabled);
+    enable_seq_trigger(seq_target.enabled);
+}
+
+static void disarm_seq(void)
+{
+    set_armed_status(&seq_target.armed, false);
+    enable_seq_trigger(false);
+}
+
+
+/* Arming and firing of DDR and BUF triggers to hardware need to be
+ * simultaneous, so are gathered in this function. */
+static void arm_hardware(bool arm_ddr, bool arm_buf, bool external)
+{
+    hw_write_trg_arm(arm_ddr, arm_buf);
+    if (!external)
+        hw_write_trg_soft_trigger(arm_ddr, arm_buf);
+}
+
+
+/* Update the reported status for the three trigger targets as part of event
+ * processing. */
+static void update_target_status(bool ddr_ready, bool buf_ready, bool seq_ready)
+{
+    if (ddr_ready)  disarm_ddr();
+    if (buf_ready)  disarm_buf();
+    if (seq_ready)  disarm_seq();
+}
+
+
+static void publish_targets(void)
+{
+    publish_status("TRG:DDR:STATUS", &ddr_target.armed);
+    publish_status("TRG:BUF:STATUS", &buf_target.armed);
+    publish_status("TRG:SEQ:STATUS", &seq_target.armed);
+    PUBLISH_WRITE_VAR_P(mbbo, "TRG:DDR:SEL", ddr_target.source_select);
+    PUBLISH_WRITE_VAR_P(mbbo, "TRG:BUF:SEL", buf_target.source_select);
+    PUBLISH_WRITE_VAR_P(bo,   "TRG:SEQ:ENA", seq_target.enabled);
+}
 
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 /* Trigger sources (two software, one external). */
-
-
-/* Updates reported armed status for this trigger source to EPICS. */
-static void set_source_armed(struct trigger_source *source, bool armed)
-{
-    source->armed = armed;
-    trigger_record(source->status_pv, 0, NULL);
-}
 
 
 /* This is the starting point for all triggering.  This is called directly by
@@ -110,33 +198,36 @@ static void arm_and_fire(struct trigger_source *source)
 {
     /* If we're already armed or if we're not actually configured to act on any
      * sources then arming is a no-op. */
-    if (source->armed)
+    if (source->armed.armed)
         return;
 
-    /* Inform all the possible targets that we're ready. */
-    bool arm_ddr = maybe_arm_target(&ddr_target, source);
-    bool arm_buf = maybe_arm_target(&buf_target, source);
-    bool arm_seq = maybe_arm_sequencer(source);
+    /* Work out which targets we're going to fire. */
+    bool external = source->source_id == TRIG_SOURCE_EXTERNAL;
+    bool arm_ddr = source->source_id == ddr_target.source_select;
+    bool arm_buf = source->source_id == buf_target.source_select;
+    bool arm_seq = arm_buf  &&  seq_target.enabled;
+
+    /* Do nothing more if there are no takers. */
     if (!arm_ddr  &&  !arm_buf  &&  !arm_seq)
-        /* If no takers then there's nothing more to do, don't actually arm
-         * at this point. */
         return;
 
-    /* Configure trigger sources we're controlling as appropriate. */
-    bool external = source->source == TRIG_SOURCE_EXTERNAL;
-    if (arm_ddr)    hw_write_ddr_trigger_select(external);
-    if (arm_buf)    hw_write_buf_trigger_select(external);
+    /* Prepare the targets that need arming. */
+    if (arm_ddr)
+        do_arm_ddr(external, !source->retrigger);
+    if (arm_buf)
+    {
+        do_arm_buf(external);
+        maybe_arm_seq();
+    }
 
     /* Hardware now ready, let's go. */
-    hw_write_trg_arm(arm_ddr, arm_buf);
-    if (!external)
-        hw_write_trg_soft_trigger(arm_ddr, arm_buf);
+    arm_hardware(arm_ddr, arm_buf, external);
 
     /* Update reported status and record which reports we expect. */
     source->ddr_armed = arm_ddr;
     source->buf_armed = arm_buf;
     source->seq_armed = arm_seq;
-    set_source_armed(source, true);
+    set_armed_status(&source->armed, true);
 }
 
 static bool call_arm_and_fire(void *context, const bool *value)
@@ -148,26 +239,34 @@ static bool call_arm_and_fire(void *context, const bool *value)
 }
 
 
-/* Called when the corresponding target has completed processing. */
-static void notify_target_ready(
-    struct trigger_source *source, struct target_status *target)
+/* Updates the status of a trigger source: if all of its targets have finished
+ * processing then the source is ready, and can retrigger if appropriate. */
+static void update_source(struct trigger_source *source)
 {
-    /* Clear the appropriate target flag. */
-    if (target == &ddr_target.status  &&  source->ddr_armed)
-        source->ddr_armed = false;
-    if (target == &buf_target.status  &&  source->buf_armed)
-        source->buf_armed = false;
-    if (target == &seq_target  &&  source->seq_armed)
-        source->seq_armed = false;
-
-    /* Check if we're still waiting for something. */
-    bool armed = source->ddr_armed || source->buf_armed || source->seq_armed;
-    if (source->armed  &&  !armed)
+    if (source->armed.armed)
     {
-        set_source_armed(source, false);
-        if (source->retrigger)
-            arm_and_fire(source);
+        bool busy =
+            (source->ddr_armed  &&  ddr_target.armed.armed)  ||
+            (source->buf_armed  &&  buf_target.armed.armed)  ||
+            (source->seq_armed  &&  seq_target.armed.armed);
+        if (!busy)
+        {
+            /* We're not busy, so mark ourself as no longer armed ... but we
+             * may also retrigger if appropriate. */
+            set_armed_status(&source->armed, false);
+            if (source->retrigger)
+                arm_and_fire(source);
+        }
     }
+}
+
+
+/* Allows all three sources to update their status.  Called periodically. */
+static void update_source_status(void)
+{
+    update_source(&s1_source);
+    update_source(&s2_source);
+    update_source(&ext_source);
 }
 
 
@@ -206,7 +305,7 @@ static bool call_reset_source(void *context, const bool *value)
 static void publish_source(const char *name, struct trigger_source *source)
 {
     const char *arm_name =
-        source->source == TRIG_SOURCE_EXTERNAL ? "ARM" : "FIRE";
+        source->source_id == TRIG_SOURCE_EXTERNAL ? "ARM" : "FIRE";
     char buffer[20];
 #define FORMAT(field) \
     (sprintf(buffer, "TRG:%s:%s", name, field), buffer)
@@ -214,94 +313,23 @@ static void publish_source(const char *name, struct trigger_source *source)
     PUBLISH_WRITE_VAR_P(bo, FORMAT("MODE"), source->retrigger);
     PUBLISH(bo, FORMAT(arm_name), call_arm_and_fire, .context = source);
     PUBLISH(bo, FORMAT("RESET"),  call_reset_source, .context = source);
-    source->status_pv = PUBLISH_READ_VAR_I(bi, FORMAT("STATUS"), source->armed);
+    publish_status(FORMAT("STATUS"), &source->armed);
 
 #undef FORMAT
 }
 
 
-
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-/* Trigger targets (DDR/BUF/SEQ). */
-
-static void set_target_armed(
-    struct target_status *status, struct trigger_source *source)
+static void publish_sources(void)
 {
-    status->current_source = source;
-    status->armed = source != NULL;
-    trigger_record(status->status_pv, 0, NULL);
+    publish_source("S1",  &s1_source);
+    publish_source("S2",  &s2_source);
+    publish_source("EXT", &ext_source);
 }
 
-
-/* Called by a trigger source to ask whether the target should be armed. */
-static bool maybe_arm_target(
-    struct trigger_target *target, struct trigger_source *source)
-{
-    if (source->source == target->source)
-    {
-        set_target_armed(&target->status, source);
-        if (target->notify)
-            target->notify();
-        return true;
-    }
-    else
-        return false;
-}
-
-
-static bool maybe_arm_sequencer(struct trigger_source *source)
-{
-    if (source->source == buf_target.source)
-    {
-        /* The associated trigger will have been armed so need to enable or
-         * disable the sequencer as appropriate. */
-        set_target_armed(&seq_target, seq_enable ? source : NULL);
-        enable_seq_trigger(seq_enable);
-        return seq_enable;
-    }
-    else
-        return false;
-}
-
-
-/* Called when the corresponding target is armed and has successfully completed
- * processing.  Updates its own status and passes the readiness indication up to
- * the corresponding trigger source. */
-static void process_target_ready(struct target_status *status)
-{
-    if (status->current_source)
-    {
-        /* Let the source know we're done.  Note that we might get retriggered
-         * as part of the notify call! */
-        struct trigger_source *source = status->current_source;
-        set_target_armed(status, NULL);
-        notify_target_ready(source, status);
-    }
-}
-
-
-static void publish_status(const char *name, struct target_status *status)
-{
-    char buffer[20];
-#define FORMAT(field) \
-    (sprintf(buffer, "TRG:%s:%s", name, field), buffer)
-
-    status->status_pv = PUBLISH_READ_VAR_I(bi, FORMAT("STATUS"), status->armed);
-
-#undef FORMAT
-}
 
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 /* Event processing and polling. */
-
-
-/* Determines whether the trigger was for a one-shot trigger: the DDR
- * processing code cares about this detail. */
-static bool is_one_shot(struct target_status *target)
-{
-    return target->current_source  &&  !target->current_source->retrigger;
-}
 
 
 /* This thread monitors the hardware for events and dispatches them as
@@ -318,23 +346,19 @@ static void *monitor_events(void *context)
 
         LOCK();
 
-        bool buf_ready = buf_target.status.armed  &&  !poll_buf_busy();
-        bool seq_ready = seq_target.armed  &&  !hw_read_seq_status();
+        bool buf_ready = buf_target.armed.armed  &&  !poll_buf_busy();
+        bool seq_ready = seq_target.armed.armed  &&  !hw_read_seq_status();
 
         /* Allow all the corresponding targets to be processed. */
         if (ddr_ready)
-            process_ddr_buffer(is_one_shot(&ddr_target.status));
+            process_ddr_buffer(ddr_target.one_shot);
         if (buf_ready)
             process_fast_buffer();
 
-        /* Retrigger as appropriate.  Note that this must come after target
-         * trigger processing to avoid loss of data. */
-        if (ddr_ready)
-            process_target_ready(&ddr_target.status);
-        if (buf_ready)
-            process_target_ready(&buf_target.status);
-        if (seq_ready)
-            process_target_ready(&seq_target);
+        /* Clear the busy flags for all targets that have completed. */
+        update_target_status(ddr_ready, buf_ready, seq_ready);
+        /* Let the trigger sources update their status and maybe retrigger. */
+        update_source_status();
 
         UNLOCK();
         /* Now sleep for a bit; 50ms seems responsive enough (20Hz). */
@@ -347,16 +371,9 @@ static void *monitor_events(void *context)
 
 bool initialise_triggers(void)
 {
-    publish_source("S1",  &s1_source);
-    publish_source("S2",  &s2_source);
-    publish_source("EXT", &ext_source);
+    publish_sources();
+    publish_targets();
 
-    publish_status("DDR", &ddr_target.status);
-    publish_status("BUF", &buf_target.status);
-    publish_status("SEQ", &seq_target);
-    PUBLISH_WRITE_VAR_P(mbbo, "TRG:DDR:SEL", ddr_target.source);
-    PUBLISH_WRITE_VAR_P(mbbo, "TRG:BUF:SEL", buf_target.source);
-    PUBLISH_WRITE_VAR_P(bo,   "TRG:SEQ:ENA", seq_enable);
     PUBLISH_READER(longin, "TRG:RAWPHASE", hw_read_trg_raw_phase);
 
     pthread_t thread_id;
