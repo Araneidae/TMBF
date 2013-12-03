Araneidae/TMBF

commit 409fb45573da58dc7d6a07d33d45397d5b40d777

TMBF: Alias and tune axis change
Add TUNE alias for TUNE:TUNE and add timebase axis to tune with timebase
in turns.  For consistency rework hardware interface to be in uncorrected
counts and do correction in hardware writing layer.

git-svn-id: svn+ssh://serv2/home/subversion/repos/controls/diamond/trunk/ioc/TMBF@103231 e099a375-04f9-0310-9d5f-a741eaff62e1

tmbfApp/src/detector.c
@@ -31,8 +31,8 @@ static bool detector_mode;
 
 /* Each time a sequencer setting changes we'll need to recompute the scale. */
 static bool tune_scale_needs_refresh = true;
-static struct epics_record *tune_scale_pv;
-static int linear_scale[TUNE_LENGTH];
+static int timebase[TUNE_LENGTH];
+static struct epics_interlock *tune_scale_trigger;
 
 /* Overflow status from the last sweep. */
 static bool overflows[OVERFLOW_BIT_COUNT];
@@ -158,20 +158,29 @@ static void update_det_scale(void)
     compute_delay();
 
     int ix = 0;
+    int total_time = 0;     // Accumulates captured timebase
+    int gap_time = 0;       // Accumulates non captured time
     unsigned int f0 = 0;
     for ( ; state > 0  &&  ix < TUNE_LENGTH; state --)
     {
         const struct seq_entry *entry = &sequencer_table[state];
+        int dwell_time = entry->dwell_time + entry->holdoff;
         if (entry->write_enable)
         {
             f0 = entry->start_freq;
+            total_time += gap_time;
+            gap_time = 0;
             for (unsigned int i = 0;
-                 i < entry->capture_count + 1  &&  ix < TUNE_LENGTH; i ++)
+                 i < entry->capture_count  &&  ix < TUNE_LENGTH; i ++, ix ++)
             {
-                store_one_tune_freq(f0, ix++);
+                store_one_tune_freq(f0, ix);
                 f0 += entry->delta_freq;
+                total_time += dwell_time;
+                timebase[ix] = total_time;
             }
         }
+        else
+            gap_time += dwell_time * entry->capture_count;
     }
 
     /* Record how many points will actually be captured. */
@@ -179,8 +188,12 @@ static void update_det_scale(void)
 
     /* Pad the rest of the scale.  The last frequency is a good a choice as any,
      * anything that goes here is invalid. */
-    while (ix < TUNE_LENGTH)
-        store_one_tune_freq(f0, ix++);
+    for ( ; ix < TUNE_LENGTH; ix ++)
+    {
+        store_one_tune_freq(f0, ix);
+        timebase[ix] = total_time;
+    }
+
 
     tune_scale_needs_refresh = false;
 }
@@ -310,8 +323,9 @@ void update_iq(const short buffer_low[], const short buffer_high[])
 {
     if (tune_scale_needs_refresh)
     {
+        interlock_wait(tune_scale_trigger);
         update_det_scale();
-        trigger_record(tune_scale_pv, 0, NULL);
+        interlock_signal(tune_scale_trigger, NULL);
     }
 
     interlock_wait(iq_trigger);
@@ -416,9 +430,11 @@ bool initialise_detector(void)
     publish_channel("M", &sweep_info.mean);
     iq_trigger = create_interlock("DET:TRIG", "DET:DONE", false);
 
-    tune_scale_pv = PUBLISH_WF_READ_VAR_I(
+    PUBLISH_WF_READ_VAR(
         double, "DET:SCALE", TUNE_LENGTH, sweep_info.tune_scale);
-    PUBLISH_WF_READ_VAR(int, "DET:LINEAR", TUNE_LENGTH, linear_scale);
+    PUBLISH_WF_READ_VAR(int, "DET:TIMEBASE", TUNE_LENGTH, timebase);
+    tune_scale_trigger =
+        create_interlock("DET:SCALE:TRIG", "DET:SCALE:DONE", false);
 
     PUBLISH_WRITER_P(ao,   "NCO:FREQ", write_nco_freq);
     PUBLISH_WRITER_P(mbbo, "NCO:GAIN", hw_write_nco_gain);
@@ -427,9 +443,6 @@ bool initialise_detector(void)
      *  wf_scaling * 2^wf_shift = 936 * 2^-32. */
     compute_scaling(BUNCHES_PER_TURN, &wf_scaling, &wf_shift);
     wf_shift -= 32;     // Divide by 2^32.
-    /* Initialise the linear scale. */
-    for (int i = 0; i < TUNE_LENGTH; i ++)
-        linear_scale[i] = i;
 
     /* Program the sequencer window. */
     PUBLISH_WAVEFORM(

tmbfApp/src/hardware.c
@@ -598,16 +598,16 @@ void hw_write_seq_entries(struct seq_entry entries[MAX_SEQUENCER_COUNT])
         struct seq_entry *entry = &entries[i];
         config_space->sequencer_write = entry->start_freq;
         config_space->sequencer_write = entry->delta_freq;
-        config_space->sequencer_write = entry->dwell_time;
+        config_space->sequencer_write = entry->dwell_time - 1;
         config_space->sequencer_write =
-            (entry->capture_count & 0xFFF) |        // bits 11:0
+            ((entry->capture_count - 1) & 0xFFF) |  // bits 11:0
             (entry->bunch_bank & 0x3) << 12 |       //      13:12
             (entry->hom_gain & 0x7) << 14 |         //      16:14
             entry->hom_enable << 17 |               //      17
             entry->enable_window << 18 |            //      18
             entry->write_enable << 19;              //      19
         config_space->sequencer_write = entry->window_rate;
-        config_space->sequencer_write = entry->holdoff & 0xFFFF;
+        config_space->sequencer_write = (entry->holdoff - 1) & 0xFFFF;
         config_space->sequencer_write = 0;
         config_space->sequencer_write = 0;
     }

tmbfApp/src/sequencer.c
@@ -125,14 +125,14 @@ static void write_seq_state(void)
         struct seq_entry *entry = &current_sequencer[i];
         entry->start_freq = tune_to_freq(bank->start_freq);
         entry->delta_freq = tune_to_freq(bank->delta_freq);
-        entry->dwell_time = bank->dwell_time - 1;
-        entry->capture_count = bank->capture_count - 1;
+        entry->dwell_time = bank->dwell_time;
+        entry->capture_count = bank->capture_count;
         entry->bunch_bank = bank->bunch_bank;
         entry->hom_gain = bank->hom_gain;
         entry->hom_enable = bank->hom_gain < HOM_GAIN_OFF;
         entry->enable_window = bank->enable_window;
         entry->write_enable = bank->write_enable;
-        entry->holdoff = bank->holdoff - 1;
+        entry->holdoff = bank->holdoff;
         entry->window_rate =
             (unsigned int) lround((pow(2, 32) / 234) / bank->dwell_time);
     }

opi/tmbf/tune.edl
@@ -3,10 +3,10 @@ beginScreenProperties
 major 4
 minor 0
 release 1
-x 1067
-y 435
+x 554
+y 150
 w 641
-h 597
+h 593
 font "helvetica-medium-r-18.0"
 ctlFont "helvetica-medium-r-18.0"
 btnFont "helvetica-medium-r-18.0"
@@ -25,6 +25,21 @@ snapToGrid
 gridSize 8
 disableScroll
 endScreenProperties
+
+# (Rectangle)
+object activeRectangleClass
+beginObjectProperties
+major 4
+minor 0
+release 0
+x 400
+y 240
+w 232
+h 48
+lineColor index 14
+fill
+fillColor index 4
+endObjectProperties
 
 # (Rectangle)
 object activeRectangleClass
@@ -63,9 +78,9 @@ major 4
 minor 0
 release 0
 x 400
-y 240
+y 304
 w 232
-h 184
+h 168
 lineColor index 14
 fill
 fillColor index 4
@@ -115,12 +130,12 @@ x 568
 y 560
 w 64
 h 24
-fgColor index 25
+fgColor index 46
 bgColor index 3
 topShadowColor index 1
 botShadowColor index 11
 label "EXIT"
-font "helvetica-medium-r-12.0"
+font "helvetica-bold-r-12.0"
 3d
 endObjectProperties
 
@@ -154,7 +169,7 @@ major 4
 minor 0
 release 0
 x 496
-y 256
+y 320
 w 104
 h 24
 fgColor index 14
@@ -173,7 +188,7 @@ major 4
 minor 1
 release 1
 x 408
-y 288
+y 352
 w 79
 h 14
 font "helvetica-bold-r-12.0"
@@ -193,7 +208,7 @@ major 4
 minor 0
 release 0
 x 552
-y 312
+y 376
 w 72
 h 24
 fgColor index 14
@@ -212,7 +227,7 @@ major 4
 minor 0
 release 0
 x 504
-y 344
+y 408
 w 96
 h 22
 fgColor index 14
@@ -231,7 +246,7 @@ major 4
 minor 1
 release 1
 x 408
-y 344
+y 408
 w 89
 h 14
 font "helvetica-bold-r-12.0"
@@ -407,7 +422,7 @@ major 4
 minor 0
 release 0
 x 448
-y 312
+y 376
 w 96
 h 24
 fgColor index 14
@@ -425,9 +440,9 @@ beginObjectProperties
 major 4
 minor 4
 release 0
-x 560
+x 544
 y 528
-w 72
+w 88
 h 24
 fgColor index 43
 bgColor index 4
@@ -449,7 +464,7 @@ major 4
 minor 6
 release 0
 x 496
-y 288
+y 352
 w 56
 h 20
 controlPv "$(device):TUNE:BUNCH_S"
@@ -561,7 +576,7 @@ major 4
 minor 0
 release 0
 x 240
-y 240
+y 232
 w 16
 h 16
 lineColor index 14
@@ -578,7 +593,7 @@ major 4
 minor 1
 release 0
 x 240
-y 264
+y 256
 w 104
 h 24
 fgColor index 25
@@ -601,7 +616,7 @@ major 4
 minor 1
 release 1
 x 408
-y 256
+y 320
 w 77
 h 14
 font "helvetica-bold-r-12.0"
@@ -621,7 +636,7 @@ major 4
 minor 1
 release 1
 x 408
-y 312
+y 376
 w 31
 h 14
 font "helvetica-bold-r-12.0"
@@ -689,7 +704,7 @@ major 4
 minor 1
 release 1
 x 400
-y 232
+y 296
 w 120
 h 16
 font "helvetica-medium-r-12.0"
@@ -708,7 +723,7 @@ major 4
 minor 0
 release 0
 x 400
-y 232
+y 296
 w 120
 h 16
 lineColor index 14
@@ -815,7 +830,7 @@ major 4
 minor 6
 release 0
 x 264
-y 240
+y 232
 w 112
 h 16
 controlPv "$(device):TUNE:SETTING"
@@ -1051,7 +1066,7 @@ major 4
 minor 0
 release 0
 x 504
-y 376
+y 440
 w 96
 h 24
 fgColor index 14
@@ -1070,7 +1085,7 @@ major 4
 minor 1
 release 1
 x 408
-y 376
+y 440
 w 52
 h 28
 font "helvetica-bold-r-12.0"
@@ -1115,9 +1130,9 @@ beginObjectProperties
 major 4
 minor 4
 release 0
-x 240
-y 296
-w 72
+x 544
+y 496
+w 88
 h 24
 fgColor index 43
 bgColor index 4
@@ -1360,9 +1375,9 @@ beginObjectProperties
 major 4
 minor 1
 release 0
-x 288
-y 328
-w 104
+x 544
+y 256
+w 80
 h 24
 fgColor index 14
 bgColor index 3
@@ -1417,9 +1432,9 @@ beginObjectProperties
 major 4
 minor 1
 release 0
-x 288
-y 360
-w 104
+x 424
+y 256
+w 96
 h 24
 fgColor index 14
 bgColor index 3
@@ -1429,7 +1444,7 @@ font "helvetica-medium-r-12.0"
 numItems 2
 symbolTag {
   0 "Frequency"
-  1 "Points"
+  1 "Timebase"
 }
 symbol0 {
   0 "x_axis"
@@ -1437,7 +1452,80 @@ symbol0 {
 }
 value0 {
   0 "SCALE"
-  1 "LINEAR"
+  1 "TIMEBASE"
 }
+endObjectProperties
+
+# (Static Text)
+object activeXTextClass
+beginObjectProperties
+major 4
+minor 1
+release 1
+x 400
+y 232
+w 120
+h 16
+font "helvetica-medium-r-12.0"
+fontAlign "center"
+fgColor index 1
+bgColor index 10
+value {
+  "Axis Selection"
+}
+endObjectProperties
+
+# (Rectangle)
+object activeRectangleClass
+beginObjectProperties
+major 4
+minor 0
+release 0
+x 400
+y 232
+w 120
+h 16
+lineColor index 14
+fillColor index 0
+endObjectProperties
+
+# (Static Text)
+object activeXTextClass
+beginObjectProperties
+major 4
+minor 1
+release 1
+x 408
+y 256
+w 24
+h 16
+font "helvetica-bold-r-12.0"
+fgColor index 14
+bgColor index 0
+useDisplayBg
+value {
+  "X:"
+}
+autoSize
+endObjectProperties
+
+# (Static Text)
+object activeXTextClass
+beginObjectProperties
+major 4
+minor 1
+release 1
+x 528
+y 256
+w 32
+h 16
+font "helvetica-bold-r-12.0"
+fgColor index 14
+bgColor index 0
+useDisplayBg
+value {
+  "Y:"
+}
+autoSize
 endObjectProperties
 

tmbfApp/Db/detector.py
@@ -21,12 +21,12 @@ for bunch in range(4):
         DESC = 'Selected bunch #%d' % bunch)
 
 
-# This will be reprocessed when necessary, basically when the sequencer settings
-# change and a detector trigger occurs.
-Waveform('DET:SCALE', TUNE_LENGTH, 'DOUBLE',
-    SCAN = 'I/O Intr', DESC = 'Scale for frequency sweep')
-# A linear scale for convenience
-Waveform('DET:LINEAR', TUNE_LENGTH, 'LONG', DESC = 'Linear scale')
+# The frequency and timebase scales will be reprocessed when necessary,
+# basically when the sequencer settings change and a detector trigger occurs.
+Trigger('DET:SCALE',
+    Waveform('DET:SCALE', TUNE_LENGTH, 'DOUBLE',
+        DESC = 'Scale for frequency sweep'),
+    Waveform('DET:TIMEBASE', TUNE_LENGTH, 'LONG', DESC = 'Timebase scale'))
 
 # Two overflow detection bits are generated
 overflows = [

tmbfApp/Db/tune.py
@@ -23,6 +23,7 @@ boolIn('TUNE:SETTING', 'Changed', 'As Set',
     ZSV = 'MINOR', OSV = 'NO_ALARM', SCAN = 'I/O Intr',
     DESC = 'Tune setup state check')
 
+tune_measurement = aIn('TUNE:TUNE', 0, 1, PREC = 4, DESC = 'Measured tune')
 Trigger('TUNE',
     # Readouts for the selected tune control.  These are copies of the
     # appropriate detector waveforms.
@@ -36,7 +37,7 @@ Trigger('TUNE',
     Waveform('TUNE:CUMSUMQ', TUNE_LENGTH, 'LONG', DESC = 'Cumsum Q'),
 
     # Tune measurement
-    aIn('TUNE:TUNE', 0, 1, PREC = 4, DESC = 'Measured tune'),
+    tune_measurement,
     aIn('TUNE:PHASE', -180, 180, 'deg', PREC = 1, DESC = 'Measured tune phase'),
     mbbIn('TUNE:STATUS',
         ('Invalid',     0,  'INVALID'),
@@ -53,3 +54,7 @@ Trigger('TUNE',
 aOut('TUNE:THRESHOLD', 0, 1, PREC = 2, DESC = 'Peak fraction threshold')
 longOut('TUNE:BLK:SEP', DESC = 'Minimum block separation')
 longOut('TUNE:BLK:LEN', DESC = 'Minimum block length')
+
+
+# Tune measurement alias for backwards compatibility
+tune_measurement.add_alias('$(DEVICE):TUNE')

commit e0e869cc5e729290fd10e37abb333c68f0b08422

TMBF: Fix bug in sequencer setup
Commit 409fb45573da58dc7d6a07d33d45397d5b40d777 (svn 103231) introduced a
bug in the configuration of the quiescent state of the sequencer.  For
correct behaviour it is necessary for all of this state, except for the
bunch bank, to be written as zeros.

git-svn-id: svn+ssh://serv2/home/subversion/repos/controls/diamond/trunk/ioc/TMBF@103561 e099a375-04f9-0310-9d5f-a741eaff62e1

tmbfApp/src/detector.c
@@ -150,11 +150,9 @@ static void store_one_tune_freq(unsigned int freq, int ix)
 
 /* Computes frequency scale directly from sequencer settings.  Triggered
  * whenever the sequencer state changes. */
-static void update_det_scale(void)
+static void update_det_scale(
+    unsigned int state, const struct seq_entry *sequencer_table)
 {
-    unsigned int state;
-    const struct seq_entry *sequencer_table = read_sequencer_table(&state);
-
     compute_delay();
 
     int ix = 0;
@@ -163,7 +161,7 @@ static void update_det_scale(void)
     unsigned int f0 = 0;
     for ( ; state > 0  &&  ix < TUNE_LENGTH; state --)
     {
-        const struct seq_entry *entry = &sequencer_table[state];
+        const struct seq_entry *entry = &sequencer_table[state - 1];
         int dwell_time = entry->dwell_time + entry->holdoff;
         if (entry->write_enable)
         {
@@ -319,12 +317,14 @@ static bool update_overflow(void)
  * according to our current configuration into separate IQ wavforms.  One
  * separate I/Q value is extracted from each channel and rotated to compensate
  * for the precomputed group delay, and an average is also stored. */
-void update_iq(const short buffer_low[], const short buffer_high[])
+void update_iq(
+    const short buffer_low[], const short buffer_high[],
+    unsigned int sequencer_pc, const struct seq_entry *sequencer_table)
 {
     if (tune_scale_needs_refresh)
     {
         interlock_wait(tune_scale_trigger);
-        update_det_scale();
+        update_det_scale(sequencer_pc, sequencer_table);
         interlock_signal(tune_scale_trigger, NULL);
     }
 

tmbfApp/src/detector.h
@@ -35,4 +35,6 @@ unsigned int tune_to_freq(double tune);
 
 /* Called on completion of buffer processing in IQ mode.  The packed I and Q
  * components are passed through for processing by the detector. */
-void update_iq(const short buffer_low[], const short buffer_high[]);
+void update_iq(
+    const short buffer_low[], const short buffer_high[],
+    unsigned int sequencer_pc, const struct seq_entry *sequencer_table);

tmbfApp/src/hardware.c
@@ -589,10 +589,23 @@ void hw_read_det_delays(int *adc_delay, int *fir_delay)
 /* * * * * * * * * * * * * * * * * * * * * */
 /* SEQ: Programmed Bunch and Sweep Control */
 
-void hw_write_seq_entries(struct seq_entry entries[MAX_SEQUENCER_COUNT])
+void hw_write_seq_entries(
+    unsigned int bank0, struct seq_entry entries[MAX_SEQUENCER_COUNT])
 {
     LOCK();
     config_space->sequencer_start_write = 0;    // Select seq state file
+    /* State zero is special: everything except the bank selection must be
+     * written as zero.  Unfortunately, these aren't the zeros in the seq_entry,
+     * so we have to write this out. */
+    config_space->sequencer_write = 0;
+    config_space->sequencer_write = 0;
+    config_space->sequencer_write = 0;
+    config_space->sequencer_write = (bank0 & 0x3) << 12;
+    config_space->sequencer_write = 0;
+    config_space->sequencer_write = 0;
+    config_space->sequencer_write = 0;
+    config_space->sequencer_write = 0;
+
     for (int i = 0; i < MAX_SEQUENCER_COUNT; i ++)
     {
         struct seq_entry *entry = &entries[i];

tmbfApp/src/hardware.h
@@ -8,7 +8,7 @@
 /* Some standard array size definitions. */
 #define BUNCHES_PER_TURN    936     // Bunches in a single turn
 #define RAW_BUF_DATA_LENGTH 16384   // Points in internal fast buffer
-#define MAX_SEQUENCER_COUNT 8       // Steps in sequencer
+#define MAX_SEQUENCER_COUNT 7       // Steps in sequencer, not counting state 0
 #define FIR_BANKS           4
 #define BUNCH_BANKS         4
 
@@ -208,7 +208,8 @@ struct seq_entry {
 };
 
 /* Rewrites the sequencer table.  All entries must be present. */
-void hw_write_seq_entries(struct seq_entry entries[MAX_SEQUENCER_COUNT]);
+void hw_write_seq_entries(
+    unsigned int bank0, struct seq_entry entries[MAX_SEQUENCER_COUNT]);
 
 /* Programs sequencer program counter.  The sequencer will run the next time the
  * buffer is armed. */

tmbfApp/src/sequencer.c
@@ -37,7 +37,8 @@ struct sequencer_bank {
 };
 
 /* Sequencer state as currently seen through EPICS. */
-static struct sequencer_bank banks[MAX_SEQUENCER_COUNT - 1];
+static struct sequencer_bank banks[MAX_SEQUENCER_COUNT];
+static unsigned int bank0;
 
 /* Sequencer state as currently written to hardware, or as will be written at
  * start of next trigger. */
@@ -119,9 +120,9 @@ static void update_capture_count(void)
  * hardware and updates hardware. */
 static void write_seq_state(void)
 {
-    for (int i = 1; i < MAX_SEQUENCER_COUNT; i ++)
+    for (int i = 0; i < MAX_SEQUENCER_COUNT; i ++)
     {
-        const struct sequencer_bank *bank = &banks[i - 1];
+        const struct sequencer_bank *bank = &banks[i];
         struct seq_entry *entry = &current_sequencer[i];
         entry->start_freq = tune_to_freq(bank->start_freq);
         entry->delta_freq = tune_to_freq(bank->delta_freq);
@@ -136,16 +137,16 @@ static void write_seq_state(void)
         entry->window_rate =
             (unsigned int) lround((pow(2, 32) / 234) / bank->dwell_time);
     }
-    hw_write_seq_entries(current_sequencer);
+    hw_write_seq_entries(bank0, current_sequencer);
 }
 
 
 static void update_seq_state(void)
 {
     /* Update all the end frequencies. */
-    for (int i = 1; i < MAX_SEQUENCER_COUNT; i ++)
+    for (int i = 0; i < MAX_SEQUENCER_COUNT; i ++)
     {
-        struct sequencer_bank *bank = &banks[i - 1];
+        struct sequencer_bank *bank = &banks[i];
         double end_freq =
             bank->start_freq + bank->capture_count * bank->delta_freq;
         WRITE_OUT_RECORD(ao, bank->end_freq_rec, end_freq, false);
@@ -174,15 +175,8 @@ void prepare_sequencer(bool enable_sequencer)
 
 static void set_state0_bunch_bank(unsigned int bank)
 {
-    current_sequencer[0].bunch_bank = bank;
-    hw_write_seq_entries(current_sequencer);
-}
-
-
-const struct seq_entry *read_sequencer_table(unsigned int *state_count)
-{
-    *state_count = sequencer_pc;
-    return current_sequencer;
+    bank0 = bank;
+    hw_write_seq_entries(bank0, current_sequencer);
 }
 
 
@@ -196,7 +190,7 @@ void process_fast_buffer(void)
     interlock_signal(buffer_trigger, NULL);
 
     if (capture_iq)
-        update_iq(buffer_low, buffer_high);
+        update_iq(buffer_low, buffer_high, sequencer_pc, current_sequencer);
 }
 
 

tmbfApp/src/sequencer.h
@@ -7,7 +7,3 @@ void process_fast_buffer(void);
 
 /* Called immediately before arming the sequencer. */
 void prepare_sequencer(bool enable_sequencer);
-
-/* Returns currently programmed sequencer table and current sequencer state
- * count. */
-const struct seq_entry *read_sequencer_table(unsigned int *state_count);