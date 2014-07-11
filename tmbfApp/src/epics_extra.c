0bcdcc892679941f346e382bf5a2784e5eb26445
mga83 <mga83@e099a375-04f9-0310-9d5f-a741eaff62e1>
2014-07-11 12:33:55 +0000

    TMBF: Fix epics_interlock implementation
    Turns out that using a mutex for cross-thread synchronisation is wrong
    and results in undefined behaviour.  Fix this by using pthread conditions
    instead.
    
    git-svn-id: svn+ssh://serv2/home/subversion/repos/controls/diamond/trunk/ioc/TMBF@116845 e099a375-04f9-0310-9d5f-a741eaff62e1

--- tmbfApp/src/epics_extra.c
+++ tmbfApp/src/epics_extra.c
@@ -26,13 +26,10 @@
 struct epics_interlock {
     struct epics_interlock *next;   // For ready notification
     struct epics_record *trigger;   // Triggers EPICS database processing
-    pthread_mutex_t mutex;          // Locked when EPICS busy
+    pthread_mutex_t mutex;
+    pthread_cond_t signal;
+    bool busy;                      // Set while EPICS is busy
 };
-
-#define LOCK_INTERLOCK(interlock) \
-    ASSERT_0(pthread_mutex_lock(&(interlock)->mutex))
-#define UNLOCK_INTERLOCK(interlock) \
-    ASSERT_0(pthread_mutex_unlock(&(interlock)->mutex))
 
 
 /* If EPICS isn't ready yet then we need to block all callers until it is. */
@@ -44,13 +41,31 @@ static struct epics_interlock *notify_list = NULL;
 #define UNLOCK_READY()  ASSERT_0(pthread_mutex_unlock(&epics_ready_mutex))
 
 
+static void take_interlock(struct epics_interlock *interlock)
+{
+    ASSERT_0(pthread_mutex_lock(&interlock->mutex));
+    while (interlock->busy)
+        ASSERT_0(pthread_cond_wait(&interlock->signal, &interlock->mutex));
+    ASSERT_0(pthread_mutex_unlock(&interlock->mutex));
+}
+
+static void release_interlock(struct epics_interlock *interlock)
+{
+    ASSERT_0(pthread_mutex_lock(&interlock->mutex));
+    interlock->busy = false;
+    ASSERT_0(pthread_cond_signal(&interlock->signal));
+    ASSERT_0(pthread_mutex_unlock(&interlock->mutex));
+}
+
+
 void interlock_wait(struct epics_interlock *interlock)
 {
-    LOCK_INTERLOCK(interlock);
+    take_interlock(interlock);
 }
 
 void interlock_signal(struct epics_interlock *interlock, struct timespec *ts)
 {
+    interlock->busy = true;
     trigger_record(interlock->trigger, 0, ts);
 }
 
@@ -59,33 +74,40 @@ void interlock_signal(struct epics_interlock *interlock, struct timespec *ts)
 static bool interlock_done(void *context, const bool *value)
 {
     struct epics_interlock *interlock = context;
-    UNLOCK_INTERLOCK(interlock);
+    release_interlock(interlock);
     return true;
 }
+
+
+/* Puts interlock into blocked state waiting for EPICS ready event.  The
+ * interlock is chained onto the list of pending interlocks to be released once
+ * EPICS ready is signalled.  MUST be called under global lock. */
+static void receive_epics_ready(struct epics_interlock *interlock)
+{
+    interlock->busy = true;
+    interlock->next = notify_list;
+    notify_list = interlock;
+}
+
 
 struct epics_interlock *create_interlock(const char *base_name, bool set_time)
 {
     struct epics_interlock *interlock = malloc(sizeof(struct epics_interlock));
     interlock->next = NULL;
+    interlock->busy = false;
+    interlock->mutex = (pthread_mutex_t) PTHREAD_MUTEX_INITIALIZER;
+    interlock->signal = (pthread_cond_t) PTHREAD_COND_INITIALIZER;
 
     char buffer[strlen(base_name) + strlen(":TRIG") + 1];
     sprintf(buffer, "%s:TRIG", base_name);
     interlock->trigger = PUBLISH(bi, buffer,
         .read = _publish_trigger_bi, .io_intr = true, .set_time = set_time);
-    pthread_mutex_init(&interlock->mutex, NULL);
     sprintf(buffer, "%s:DONE", base_name);
     PUBLISH(bo, buffer, .write = interlock_done, .context = interlock);
 
     LOCK_READY();
     if (!epics_ready)
-    {
-        /* Start with the mutex locked if EPICS hasn't started yet and chain
-         * onto the list of pending interlocks, this will be unlocked once EPICS
-         * ready is signalled. */
-        LOCK_INTERLOCK(interlock);
-        interlock->next = notify_list;
-        notify_list = interlock;
-    }
+        receive_epics_ready(interlock);
     UNLOCK_READY();
     return interlock;
 }
@@ -95,24 +117,18 @@ void wait_for_epics_start(void)
 {
     /* This is very naughty code.  We fake up a temporary interlock object just
      * so we can go onto the initialisation list.  Still, it will serve... */
-    struct epics_interlock interlock = { .mutex = PTHREAD_MUTEX_INITIALIZER };
+    struct epics_interlock interlock = {
+        .mutex = PTHREAD_MUTEX_INITIALIZER,
+        .signal = PTHREAD_COND_INITIALIZER };
 
     LOCK_READY();
     bool lock_needed = !epics_ready;
     if (lock_needed)
-    {
-        interlock.next = notify_list;
-        notify_list = &interlock;
-        LOCK_INTERLOCK(&interlock);
-    }
+        receive_epics_ready(&interlock);
     UNLOCK_READY();
 
-    if(lock_needed)
-    {
-        LOCK_INTERLOCK(&interlock);
-        UNLOCK_INTERLOCK(&interlock);
-        pthread_mutex_destroy(&interlock.mutex);
-    }
+    if (lock_needed)
+        take_interlock(&interlock);
 }
 
 
@@ -130,7 +146,7 @@ static void init_hook(initHookState state)
              * it's possible that the current head will evaporate as soon as we
              * touch it, so grab its next pointer first for safety. */
             struct epics_interlock *next = notify_list->next;
-            UNLOCK_INTERLOCK(notify_list);
+            release_interlock(notify_list);
             notify_list = next;
         }
         UNLOCK_READY();
